5aaea983b4d4a6cbef290e808a45debaf3203bb6
idonia <sidoniag@gmail.com>
2020-09-16 12:20:45 -0500

    update api index.js

--- routes/api/index.js
+++ routes/api/index.js
@@ -1,6 +1,7 @@
 const router = require('express').Router();
 // const pizzaController = require('../../controllers/pizza-controller');
 const userRoutes = require('./user-routes');
+const thoughtRoutes = require('./thoughts-routes');
 
 // add prefix of `/user` to routes created in `user-routes.js1
 router.use('/user', userRoutes);
