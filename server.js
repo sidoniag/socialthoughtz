33db3b44a43e177577d349dd30616b9ad62ed172
idonia <sidoniag@gmail.com>
2020-09-16 12:23:51 -0500

    need debug server

--- server.js
+++ server.js
@@ -10,7 +10,7 @@ app.use(express.static('public'));
 
 app.use(require('./routes'));
 
-mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost/pizza-hunt', {
+mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost/socialthoughtz', {
     useFindAndModify: false,
     useNewUrlParser: true,
     useUnifiedTopology: true
