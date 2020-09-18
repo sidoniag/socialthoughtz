886fcb2c40a643540cae08398d3bb3e1cd8dc8a3
idonia <sidoniag@gmail.com>
2020-09-18 11:26:41 -0500

    server run no info

--- controllers/thought-controller.js
+++ controllers/thought-controller.js
@@ -2,7 +2,7 @@ const { Thought, User } = require('../models');
 
 const thoughtController = {
    // get all thoughts
-   getAllThoughts(req, res) {
+   getAllThought(req, res) {
        Thought.find({})
        .then(dbThoughtData => res.json(dbThoughtData))
        .catch(err => {
