abc7b8947ac3645307afa7f6bd4b1cde314eeb3b
idonia <sidoniag@gmail.com>
2020-09-28 22:54:45 -0500

    correct server.js

--- server.js
+++ server.js
@@ -6,10 +6,9 @@ const PORT = process.env.PORT || 3001;
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-app.use(express.static('public'));
+// app.use(express.static('public'));
 
-// app.use(require('./routes'));
-const { User } = require('./models');
+app.use(require('./routes'));
 
 mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost/socialthoughtz', {
     useFindAndModify: false,
