01789aa60b75d8c98d8a0ae465d956de8f7d6c71
idonia <sidoniag@gmail.com>
2020-10-08 12:00:49 -0500

    found user error

--- models/User.js
+++ models/User.js
@@ -1,7 +1,7 @@
 const { Schema, model } = require('mongoose');
 
 const UserSchema = new Schema({
-    userName: {
+    username: {
         type: String,
         unique: true,
         required: true,
